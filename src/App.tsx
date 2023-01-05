f465d797e6337bd31aeaf2a99f61b4c01aa4c9e0
hunjoonrhee <hunjoonrhee90@gmail.com>
2023-01-05 05:35:07 +0100

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+import {Canvas} from '@react-three/fiber';
+import {OrbitControls} from "@react-three/drei";
 import './App.css';
+import ModelAni2 from "./components/Model_ani2";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Canvas camera={{position: [2, 0, 12.25], fov: 15}}
+      style={{backgroundColor: '#111a21', width: '100vw', height: '100vh'}}>
+        <ambientLight intensity={1.25} />
+        <ambientLight intensity={0.1} />
+        <directionalLight intensity={0.4} />
+          <ModelAni2 position={[0.025, -0.9, 0]} />
+          <OrbitControls/>
+      </Canvas>
+
   );
 }
 
